e379fa975274d08a3ade4b00ae3a70fe1196267c
kaji-hiro <kajihiro190206@gmail.com>
2020-10-13 15:02:35 +0900

    fix but not ac

--- ABC179/E-Sequence_Sum.rb
+++ ABC179/E-Sequence_Sum.rb
@@ -1,29 +1,33 @@
 n, x, m = gets.split(' ').map(&:to_i)
 a = []
-a[1] = x
-sum = x
-b = [0]
+a[0] = x
+b = [nil]
 j = 0
-start = 0
-array = []
+a_sum = [x]
+b_sum = 0
+flag = false
 
-2.upto(n) do |i|
-  a[i] = a[i - 1] ** 2 % m
+1.upto(n - 1) do |i|
+  a[i] = a[i - 1] * a[i - 1] % m
+  a_sum[i] = a_sum[i - 1] + a[i]
   if b[0] == a[i]
+    flag = true
     break
   elsif a.uniq.count != a.count
     b[j] = a[i]
     j += 1
+    b_sum += a[i]
   end
 end
 
-start = a.count - b.count
-
-2.upto(start) do |i|
-  sum += a[i]
+if flag
+  a.delete_at(-1)
+  start = a.count - j * 2
+  sum = a_sum[start - 1]
+  sum += b_sum * (n - start).div(j)
+  sum += b[0..((n - start) % j - 1)].sum
+else
+  sum = a_sum[n - 1]
 end
-
-sum += b.sum * (n - start) / b.count
-sum += b[0..((n - start) % b.count)].sum
 
 puts sum
