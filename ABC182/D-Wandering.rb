fac951f37d400ef87ddd80c4e29d00b2ffd1e7f7
kaji-hiro <kajihiro190206@gmail.com>
2020-11-10 22:17:31 +0900

    D

--- ABC182/D-Wandering.rb
+++ ABC182/D-Wandering.rb
@@ -1,16 +1,13 @@
 n = gets.to_i
 a = gets.split.map(&:to_i)
-a_sum, a_sum_ans = Array.new(n), Array.new(n)
-a_sum[0], a_sum_ans[0] = a[0], a[0]
-ans = 0
-1.upto(n - 1) do |i|
-  a_sum[i] = a_sum[i - 1] + a[i]
-  a_sum_ans[i] = a_sum_ans[i - 1] + a_sum[i]
-  tmp = a_sum_ans[i - 1]
-  i.times do |j|
-    tmp += a[j]
-    ans = tmp if ans < tmp
-  end
+
+a_sum, max_sum, position, ans = 0, 0, 0, 0
+
+a.each do |a|
+  a_sum += a
+  max_sum = a_sum if max_sum < a_sum
+  ans = position + max_sum if ans < position + max_sum
+  position += a_sum
 end
 
 puts ans
